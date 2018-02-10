8ff96fbd14869fb4c0326b69a5fc8393dc700ee9
Dca <dcahsu@gmail.com>
2018-02-10 19:04:58 +0800

    remove parcel

--- src/js/main.js
+++ src/js/main.js
@@ -11,14 +11,14 @@ var app = new Vue({
   },
   methods: {
    doJieba: _.debounce(function () {
-      const _this = this
+      var _this = this
       call_jieba_cut(this.message, function (_result) {
        console.log(_result);
         _this.addedMessage = _result.join('！') + '！';
       });
     }, 300),
 	copy: function(){
-		const _input = $(".result-text")
+		var _input = $(".result-text")
 		_input.text(this.addedMessage);
 		_input.select();
 		document.execCommand("copy");
